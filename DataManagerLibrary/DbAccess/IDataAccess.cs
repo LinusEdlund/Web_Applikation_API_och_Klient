8e63b8674c6a382971ef4ebcc647433769b72ec4
Linus Edlund <Linus.Edlund@hotmail.com>
2023-05-09 17:01:46 +0200

    La till Model och dataccess med dapper

---
+++ DataManagerLibrary/DbAccess/IDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace DataManagerLibrary.DbAccess;
+
+public interface IDataAccess
+{
+    Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+    Task SaveDataAsync<T>(string storedProcedure, T parameters, string connectionId = "Default");
+}
---
+++ DataManagerLibrary/DbAccess/MySQLDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DataManagerLibrary.DbAccess;
+public class MySQLDataAccess : IDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public MySQLDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, U parameters, string connectionId = "Default")
+    {
+        using IDbConnection connection = new MySqlConnection(_config.GetConnectionString(connectionId));
+
+        return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveDataAsync<T>(string storedProcedure, T parameters, string connectionId = "Default")
+    {
+        using IDbConnection connection = new MySqlConnection(_config.GetConnectionString(connectionId));
+
+        await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+}
