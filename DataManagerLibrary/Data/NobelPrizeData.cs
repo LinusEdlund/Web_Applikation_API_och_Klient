8abf8b043045feb1e93cadb6749ff15877b9af7b
Linus Edlund <Linus.Edlund@hotmail.com>
2023-05-10 08:35:25 +0200

    Gjorde all data alternativ som behövs för det här objektet

--- DataManagerLibrary/Data/NobelPrizeData.cs
+++ DataManagerLibrary/Data/NobelPrizeData.cs
@@ -13,4 +13,23 @@ public class NobelPrizeData : INobelPrizeData
 
     public Task<IEnumerable<NobelPrizeModel>> GetAll() =>
         _db.LoadDataAsync<NobelPrizeModel, dynamic>("nobel.spNoble_GetAll", new { });
+
+    public async Task<NobelPrizeModel?> GetById(int id) 
+    {
+        var nobelPrizes = await _db.LoadDataAsync<NobelPrizeModel, dynamic>("nobel.spNobel_GetById", new { Id = id });
+
+        return nobelPrizes.FirstOrDefault();
+    }
+
+    public Task Insert(NobelPrizeModel nobel) =>
+        _db.SaveDataAsync<dynamic>(
+            "nobel.spNobel_Insert",
+            new { nobel.Year, nobel.Name, nobel.Citation, nobel.Country, nobel.Institution, nobel.Gender, nobel.Age });
+
+    public Task DeleteById(int id) =>
+        _db.SaveDataAsync("nobel.spNobel_Delete", new { Id = id });
+
+    public Task Update(NobelPrizeModel nobel) =>
+        _db.SaveDataAsync("nobel.spNobel_Update", nobel);
+        
 }
