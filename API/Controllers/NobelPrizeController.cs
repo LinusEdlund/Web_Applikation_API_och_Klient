a0c755c182969d48c21b71933a86512c24d0474f
Linus Edlund <Linus.Edlund@hotmail.com>
2023-05-10 10:21:34 +0200

    Gjorde Nobel Controllern så man kan göra crud på den kanske kan lägga till patch sen

--- API/Controllers/NobelPrizeController.cs
+++ API/Controllers/NobelPrizeController.cs
@@ -14,6 +14,7 @@ public class NobelPrizeController : ControllerBase
     {
         _data = data;
     }
+
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
@@ -26,4 +27,65 @@ public class NobelPrizeController : ControllerBase
 			return Problem(ex.Message);
 		}
     }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+		try
+		{
+			var results = await _data.GetById(id);
+			if (results is null)
+			{
+				return NotFound();
+			}
+
+			return Ok(results);
+		}
+		catch (Exception ex)
+		{
+			return Problem(ex.Message);
+		}
+    }
+
+	[HttpPost]
+	public async Task<IActionResult> Create(NobelPrizeModel model)
+	{
+		try
+		{
+			await _data.Insert(model);
+			return Ok();
+		}
+		catch (Exception ex)
+		{
+			return Problem(ex.Message);
+		}
+	}
+
+	[HttpPut]
+	public async Task<IActionResult> Update(NobelPrizeModel model)
+	{
+		try
+		{
+			await _data.Update(model);
+			return Ok();
+		}
+		catch (Exception ex)
+		{
+			return Problem(ex.Message);
+		}
+	}
+
+	[HttpDelete]
+	public async Task<IActionResult> Delete(int id)
+	{
+		try
+		{
+			await _data.DeleteById(id);
+			return Ok();
+		}
+		catch (Exception ex)
+		{
+			return Problem(ex.Message);
+		}
+	}
 }
