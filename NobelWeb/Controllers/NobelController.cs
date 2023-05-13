9536d2a9d698e01fbe0081b4d6722e79984ce6cf
Linus Edlund <Linus.Edlund@hotmail.com>
2023-05-13 15:33:31 +0200

    fix index sidan i nobel controller

---
+++ NobelWeb/Controllers/NobelController.cs
@@ -0,0 +1,20 @@
+﻿using DataManagerLibrary.Data;
+using DataManagerLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NobelWeb.Controllers;
+public class NobelController : Controller
+{
+    private readonly INobelPrizeData _nobelData;
+
+    public NobelController(INobelPrizeData nobelData)
+    {
+        _nobelData = nobelData;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var nobelList = await _nobelData.GetAll();
+        return View(nobelList.ToList());
+    }
+}
--- NobelWeb/Program.cs
+++ NobelWeb/Program.cs
@@ -1,7 +1,14 @@
+using DataManagerLibrary.Data;
+using DataManagerLibrary.DbAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<INobelPrizeData, NobelPrizeData>();
+builder.Services.AddSingleton<IDataAccess, MySQLDataAccess>();
+
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
